38ac756cec293ebc58678a22c3034066a6836ce7
devstiven12 <devstiven12@gmail.com>
2026-06-18 17:06:09 -0500

    se crean los archivos de component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,19 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
+import { CoreModule } from './core/core.module';
+import { SharedModule } from './shared/shared.module';
+import { UsersModule } from './features/users/users.module';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    CoreModule,
+    SharedModule,
+    UsersModule
   ],
   providers: [],
   bootstrap: [AppComponent]
